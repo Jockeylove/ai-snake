c8a2f7d859e0b4bc100206f0ee680b1582b5aa2e
Jockeylove <56461099+Jockeylove@users.noreply.github.com>
2019-10-16 17:13:23 +0800

    Add files via upload

--- formal snake.cpp
+++ formal snake.cpp
@@ -13,13 +13,9 @@ void color(const unsigned short textColor){
   SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), textColor);
 }
 
-bool operator==(COORD &A, COORD&B){
-	if (A.X == B.X&&A.Y == B.Y)return true;
-	return false;
-}
-
-bool operator!=(COORD &A, COORD&B) {
-	return !(A == B);
+bool operator!=(COORD &A, COORD&B){
+	if (A.X == B.X&&A.Y == B.Y)return false;
+	return true;
 }
 
 class snake {
@@ -39,7 +35,7 @@ public:
 		print(body[1], '*');
 		board[0][0] = true;
 		board[1][0] = true;
-		init_board(path_board);
+  		init_board(path_board);
 		init_path();
 		set_path_board({ 0,0 }, true);
 		create_food();
@@ -52,12 +48,30 @@ public:
 		case R:t.X+=2; move(t); break;
 		case D:t.Y++; move(t); break;
 		case U:t.Y--; move(t); break;
-		default:break;
 		}
 		Sleep(20+60/len);
 	}
 
- void print(COORD p, char c){
+	void move(COORD x) {
+		if (x != food) {
+			COORD t = body.back();
+			print(t, ' ');
+			set_board(t,false);
+			body.pop_back();
+			body.push_front(x);
+			set_board(x,true);
+			print(x, '*');
+		}
+		else {
+			print(x, '*');
+			body.push_front(x);
+			set_board(x,true);
+			create_food();
+		}del_path();
+		set_path();
+	}
+	
+ 	void print(COORD p, char c){
 	HANDLE Houtput = GetStdHandle(STD_OUTPUT_HANDLE);
 	color(C[len%5]);
 	SetConsoleCursorPosition(Houtput, p);
@@ -71,13 +85,38 @@ public:
 	printf("$ ");
 	}
 	
+	void mark(){
+		HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleCursorPosition(hout,{2*size+10,0});
+		color(7);
+		printf("蛇身长度: %d",len);
+	}
+	
 	void init_board(bool A[size][size]) {
 		for (int i = 0; i < size; i++)
 			for (int j = 0; j < size; j++)
 			A[i][j] = false;
 	}
 
-	void create_food() {
+	void init_path() {
+		for (int i = 0; i < size; i += 2) {
+			for (int j = 0; j < size; j += 2) {
+				path[i][j] = R;
+				path[i + 1][j + 1] = L;
+				path[i][j + 1] = U;
+				path[i + 1][j] = D;
+			}
+		}
+	}
+
+	void set_path_board(COORD x, bool t) {
+		path_board[x.X/2][x.Y] = t;
+		path_board[x.X/2+1][x.Y] = t;
+		path_board[x.X/2][x.Y+1] = t;
+		path_board[x.X/2+1][x.Y+1] = t;
+	}
+
+ 	void create_food() {
 		len++;
 		do{
 			food.X = rand() % size*2;
@@ -87,24 +126,16 @@ public:
 		del_path();
 		set_path();
 		mark();
-		
 	}
 
-	void mark(){
-		HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
-		SetConsoleCursorPosition(hout,{2*size+10,0});
-		color(7);
-		printf("蛇身长度: %d",len);
-	}
-	
 	void del_path() {
 		for (int i = 0; i <=size-2; i += 2) {
 			for (int j = 1; j < size-2; j += 2) {
 				if (matrix_no_point({ i*2,j - 1 }) || matrix_no_point({ i*2,j + 1 })) {
-					del_path({ i*2, j });
+					del_path_board({ i*2, j });
 				}
 				if (matrix_no_point({ (j-1)*2,i}) || matrix_no_point({ (j+1)*2,i })) {
-					del_path({ j*2, i });
+					del_path_board({ j*2, i });
 				}
 			}
 		}
@@ -134,45 +165,8 @@ public:
 		}
 	}
 
-	void move(COORD x) {
-		if (x != food) {
-			print(body.back(), ' ');
-			set_board(body.back(),false);
-			body.pop_back();
-			body.push_front(x);
-			set_board(x,true);
-			print(x, '*');
-		}
-		else {
-			print(x, '*');
-			body.push_front(x);
-			set_board(x,true);
-			create_food();
-		}del_path();
-		set_path();
-	}
-
-
-	void init_path() {
-		for (int i = 0; i < size; i += 2) {
-			for (int j = 0; j < size; j += 2) {
-				path[i][j] = R;
-				path[i + 1][j + 1] = L;
-				path[i][j + 1] = U;
-				path[i + 1][j] = D;
-			}
-		}
-	}
-
 	void set_board(COORD &x,bool t) {
 	board[x.X/2][x.Y] = t;
-	}
-	
-	void set_path_board(COORD x, bool t) {
-		path_board[x.X/2][x.Y] = t;
-		path_board[x.X/2+1][x.Y] = t;
-		path_board[x.X/2][x.Y+1] = t;
-		path_board[x.X/2+1][x.Y+1] = t;
 	}
 
 	bool matrix_no_point(COORD x) {
@@ -214,7 +208,7 @@ public:
 		}
 	}
 
-	void del_path(COORD x) {
+	void del_path_board(COORD x) {
 		if ((x.X/2) % 2) {
 			path[x.X/2][x.Y] = D;
 			path[x.X/2 + 1][x.Y + 1] = U;
